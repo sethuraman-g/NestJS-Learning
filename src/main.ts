0ab387ee55bc091d4d38715305227d874927638e
sethuraman-g <sethuraman.ganesan@numentica-ui.com>
2025-08-29 16:21:03 +0530

    Swagger UI Set up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for Learning My NestJS app')
+    .setVersion('1.0')
+    .addBearerAuth() // if you want JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(
+    `🚀 App is running on http://localhost:${process.env.PORT ?? 3000}/swagger 🔥`,
+  );
 }
 bootstrap();
